03d873c40a9e96275e958b77f6405009eee6aa44
bllackdev <rapha_rmm@hotmail.com>
2020-08-12 20:26:54 -0300

    add Fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+import { useFonts, IndieFlower_400Regular } from '@expo-google-fonts/indie-flower';
 
 export default function App() {
-  return (
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    IndieFlower_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="auto" />
+      </>
+    );
+  }
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -2,9 +2,28 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#32264D'
-  }
+    flex: 1,
+    backgroundColor: '#9871F5',
+    justifyContent: 'center',
+    padding: 40
+  },
 
+  banner: {
+    width: '100%',
+    resizeMode: 'contain'
+  },
+
+  title: {
+    fontFamily: 'IndieFlower_400Regular',
+    color: '#FFF',
+    fontSize: 20,
+    lineHeight: 30,
+    marginTop: 80,
+  },
+
+  titleBold: {
+    fontWeight: 'bold',
+  }
 });
 
 export default styles;
