d2cc064a40c39048a43e9a1e459c3d786eccfdc3
bllackdev <rapha_rmm@hotmail.com>
2020-08-12 13:16:37 -0300

    add page Landing

--- App.tsx
+++ App.tsx
@@ -1,27 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello BllackDev!</Text>
+    <>
+      <Landing />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6842C2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontSize: 32,
-    color: '#04D361',
-    fontWeight: 'bold'
-  }
-});
