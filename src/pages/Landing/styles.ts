514e4d2591ce6e2999c216b5777a7f42157692e9
bllackdev <rapha_rmm@hotmail.com>
2020-08-12 21:06:04 -0300

    finalizado o style da Landing

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9871F5',
+    backgroundColor: '#47248F',
     justifyContent: 'center',
     padding: 40
   },
@@ -23,7 +23,46 @@ const styles = StyleSheet.create({
 
   titleBold: {
     fontWeight: 'bold',
-  }
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between',
+  },
+
+  button: {
+    height: 150,
+    width: '48%',
+    backgroundColor: '#333',
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: 'space-between'
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871F5',
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04D361',
+  },
+
+  buttonText: {
+    fontFamily: 'IndieFlower_400Regular',
+    color: '#FFF',
+    fontSize: 20,
+  },
+
+  totalConnections: {
+    fontFamily: 'IndieFlower_400Regular',
+    color: '#d4c2ff',
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+    marginTop: 40,
+  },
+
 });
 
 export default styles;
