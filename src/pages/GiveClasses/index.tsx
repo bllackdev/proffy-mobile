3fcda695748bb0e5e2fd6992d89ed850c6853da6
bllackdev <rapha_rmm@hotmail.com>
2020-08-13 23:25:25 -0300

    add page GiveClasses e confingurado a rota

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,12 +1,36 @@
-import React, { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
 
+import giveClassesBgImage from '../../assets/img/give-classes-background.png'
+
+import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses() {
-  return (
-    <View>
+  const { goBack } = useNavigation();
 
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
     </View>
-  )
+  );
 }
 
 
