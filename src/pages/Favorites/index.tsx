66f7f0243b8d0f2bbb2be73921b4995a2cec5d67
bllackdev <rapha_rmm@hotmail.com>
2020-08-14 22:58:49 -0300

    add e finalizado o PageHeader

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View } from "react-native";
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function Favorires() {
   return (
-    <View style={styles.container} />
+    <View style={styles.container}>
+      <PageHeader title="Meus proffy favoritos" />
+    </View>
   );
 }
 
